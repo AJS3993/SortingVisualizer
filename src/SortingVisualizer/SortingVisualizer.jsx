c9ef41afde0c8c5c27c771ebfe45570f49fa972a
Andrew Smith <ajs3993@gmail.com>
2020-01-31 13:01:58 -0600

    data displayed as bars

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -17,30 +17,31 @@ export class SortingVisualizer extends React.Component {
     }
 
 
+// creates array, generates 100 random numbers and pushes each into that array
     resetArray(){
         const array = [];
         for (let i=0; i<100;i++) {
             array.push(randomIntFromInterval(5, 1000));
         }
+//resets the state to have this new array
         this.setState({array});
     }
 
     render(){
         const {array} = this.state
-
-        return (
-        <>
         
-            {array.map((value, idx)=> (
-                <div className='array-bar' key={idx}>
-                    {value}
-                </div>
+        return (
+            <>
+            <div className = 'array-container'>
+                {array.map((value, idx)=> (
+                    <div className='array-bar' 
+                    key={idx}
+                    style={{height: `${value}px`}}></div>
             ))}
-
+            </div>
         </>
         );
     }
-
 }
 
 //generate random number
