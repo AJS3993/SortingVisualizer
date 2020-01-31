991244486ef637e0de36c216dcb78f7f1f05bf1d
Andrew Smith <ajs3993@gmail.com>
2020-01-31 14:23:44 -0600

    merge sort and animation

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './SortingVisualizer.css';
+import {getMergeSortAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
 
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
 export class SortingVisualizer extends React.Component {
     constructor(props){
@@ -21,11 +30,36 @@ export class SortingVisualizer extends React.Component {
     resetArray(){
         const array = [];
         for (let i=0; i<100;i++) {
-            array.push(randomIntFromInterval(5, 1000));
+            array.push(randomIntFromInterval(5, 500));
         }
 //resets the state to have this new array
         this.setState({array});
     }
+
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+      }
+    
 
     render(){
         const {array} = this.state
@@ -39,6 +73,7 @@ export class SortingVisualizer extends React.Component {
                     style={{height: `${value}px`}}></div>
             ))}
             <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
             </div>
         </>
         );
